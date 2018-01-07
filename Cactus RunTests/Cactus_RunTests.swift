1927dc24a990bbbdc987311bc7b68a8963d18378
Margarita Tereshchenkova <mtereshc@gmail.com>
2018-01-07 18:24:57 -0500

    Initial Commit

---
+++ Cactus RunTests/Cactus_RunTests.swift
@@ -0,0 +1,36 @@
+//
+//  Cactus_RunTests.swift
+//  Cactus RunTests
+//
+//  Created by margot on 2018-01-07.
+//  Copyright © 2018 foxberryfields. All rights reserved.
+//
+
+import XCTest
+@testable import Cactus_Run
+
+class Cactus_RunTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
