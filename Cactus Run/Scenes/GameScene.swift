9aa71e6d8d1b2c192e30b0a4ce8fcbd58cd74ea1
Margarita Tereshchenkova <mtereshc@gmail.com>
2018-02-10 15:21:03 -0500

    add background

--- Cactus Run/Scenes/GameScene.swift
+++ Cactus Run/Scenes/GameScene.swift
@@ -8,14 +8,21 @@
 
 import SpriteKit
 
+enum GameState {
+    case ready, ongoing, paused, finished
+}
+
 class GameScene: SKScene {
     
     var worldLayer: Layer!
+    var backgroundLayer: RepeatingLayer!
     var mapNode: SKNode!
     var tileMap: SKTileMapNode!
     
     var lastTime: TimeInterval = 0
     var dt: TimeInterval = 0
+
+    var gameState = GameState.ready
     
     override func didMove(to view: SKView) {
         createLayers()
@@ -23,8 +30,24 @@ class GameScene: SKScene {
     
     func createLayers() {
         worldLayer = Layer()
+        worldLayer.zPosition = GameConstants.ZPositions.worldZ
         addChild(worldLayer)
         worldLayer.layerVelocity = CGPoint(x: -200.0, y: 0.0)
+        
+        backgroundLayer = RepeatingLayer()
+        backgroundLayer.zPosition = GameConstants.ZPositions.farBGZ
+        addChild(backgroundLayer)
+        
+        for i in 0...1{
+            let backgroundImage = SKSpriteNode(imageNamed: GameConstants.StringConstants.worldBackgroundNames[0])
+            backgroundImage.name = String(i)
+            backgroundImage.scale(to: frame.size, width: false, multiplier: 1.0)
+            backgroundImage.anchorPoint = CGPoint.zero
+            backgroundImage.position = CGPoint(x: 0.0 + CGFloat(i) * backgroundImage.size.width, y: 0.0)
+            backgroundLayer.addChild(backgroundImage)
+        }
+        
+        backgroundLayer.layerVelocity = CGPoint(x: -100.0, y: 0.0)
         
         load(level: "Level_0-1")
     }
@@ -38,10 +61,27 @@ class GameScene: SKScene {
     }
     
     func loadTileMap() {
-        if let groundTiles = mapNode.childNode(withName: "Ground Tiles") as? SKTileMapNode {
+        if let groundTiles = mapNode.childNode(withName: GameConstants.StringConstants.groundTilesName) as? SKTileMapNode {
             tileMap = groundTiles
             tileMap.scale(to: frame.size, width: false, multiplier: 1.0)
         }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        switch gameState {
+        case .ready:
+            gameState = .ongoing
+        default:
+            break
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
     }
     
     override func update(_ currentTime: TimeInterval) {
@@ -52,7 +92,11 @@ class GameScene: SKScene {
         }
         lastTime = currentTime
         
-        worldLayer.update(dt)
+        if gameState == .ongoing {
+            worldLayer.update(dt)
+            backgroundLayer.update(dt)
+            
+        }
     }
     
 }
---
+++ Cactus Run/Template/GameConstants.swift
@@ -0,0 +1,26 @@
+//
+//  GameConstants.swift
+//  Cactus Run
+//
+//  Created by margot on 2018-02-10.
+//  Copyright © 2018 foxberryfields. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+struct GameConstants {
+    struct ZPositions {
+        static let farBGZ: CGFloat = 0
+        static let closeBGZ: CGFloat = 1
+        static let worldZ: CGFloat = 2
+        static let objectZ: CGFloat = 3
+        static let playerZ: CGFloat = 4
+        static let hudZ: CGFloat = 5
+    }
+    
+    struct StringConstants {
+        static let groundTilesName = "Ground Tiles"
+        static let worldBackgroundNames = ["DesertBackground", "GrassBackground"]
+    }
+}
